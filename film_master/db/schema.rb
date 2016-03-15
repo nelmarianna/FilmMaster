d992c71fd1d74d966774017973c1c6c07f158345
Lucia <lucia.okeh@gmail.com>
2016-03-15 13:48:55 -0400

    Genre table added

--- film_master/db/schema.rb
+++ film_master/db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160315011258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", primary_key: "genreid", force: :cascade do |t|
+    t.string "gname", limit: 10
+  end
 
 end
