6cd86ec8cbd6bccd59074683957d6c8012e63201
Lucia <lucia.okeh@gmail.com>
2016-03-17 13:31:27 -0400

    Movieand rating tables

--- film_master/db/schema.rb
+++ film_master/db/schema.rb
@@ -23,6 +23,23 @@ ActiveRecord::Schema.define(version: 20160317003133) do
     t.datetime "updated_at", null: false
   end
 
+ create_table "movie", primary_key: "movieid", force: :cascade do |t|
+     t.string  "mname",         limit: 20, null: false
+     t.text    "summary",                  null: false
+     t.integer "overallrating"
+     t.string  "language",      limit: 10, null: false
+     t.date    "datereleased",             null: false
+     t.string  "maturitylevel", limit: 4,  null: false
+     t.boolean "subtitle",                 null: false
+     t.time    "duration",                 null: false
+   end
+ 
+   create_table "ratings", primary_key: "ratingid", force: :cascade do |t|
+     t.integer "movieid",      default: "nextval('ratings_movieid_seq'::regclass)", null: false
+     t.integer "rating",                                                            null: false
+     t.date    "datereleased",                                                      null: false
+   end
+
   create_table "users", primary_key: "userid", force: :cascade do |t|
     t.string   "email",                             default: "", null: false
     t.string   "encrypted_password",                default: "", null: false
@@ -43,4 +60,5 @@ ActiveRecord::Schema.define(version: 20160317003133) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+   add_foreign_key "ratings", "movie", column: "movieid", primary_key: "movieid", name: "ratings_movieid_fkey"
 end
