d95a92009965719918dbdcaf1732bbb1c1497eaa
Manisha Wanniarachchige <Manisha Wanniarachchige>
2016-03-21 19:19:14 -0400

    Lucia's tables added to my repo

--- film_master/db/schema.rb
+++ film_master/db/schema.rb
@@ -10,7 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
+#Manisha 
 
 ActiveRecord::Schema.define(version: 20160320044306) do
 
