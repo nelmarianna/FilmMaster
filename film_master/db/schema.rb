53cafc91a9f1b8d657638c72e2288f98e1d3fd4b
Manisha Wanniarachchige <Manisha Wanniarachchige>
2016-03-21 18:48:14 -0400

    updates to director and studios im serious this time

--- film_master/db/schema.rb
+++ film_master/db/schema.rb
@@ -26,38 +26,14 @@ ActiveRecord::Schema.define(version: 20160321193612) do
     t.string "lName", null: false
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "rName",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+
 
   create_table "studios", primary_key: "studio_id", force: :cascade do |t|
     t.string "studio_name", null: false
     t.string "country",     null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
+ # sdaksldj
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
