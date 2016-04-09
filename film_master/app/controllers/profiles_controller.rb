f088572f1a842550819bbfb96602bd62670c5453
nelmarianna <aniela.opolski@gmail.com>
2016-04-09 16:25:25 -0400

    RESTful profile page

--- film_master/app/controllers/profiles_controller.rb
+++ film_master/app/controllers/profiles_controller.rb
@@ -7,6 +7,23 @@ class ProfilesController < ApplicationController
 
   end
 
+	def edit
+
+		@profiles = Profile.find(params[:profile_id])
+		@users = User.find(@profiles.user_id)
+		render("edit")
+	end
+
+	def update
+		@profiles = Profile.find(params[:profile_id])
+		profile_params = params.require(:profile).permit(:gender,:birthday,:language)
+		
+		if @profiles.update(profile_params)
+			redirect_to "/profiles/#{@profiles.profile_id}" 
+		else
+			render ("edit")
+		end
+	end
   # def create
   #   @profiles = Profile.new(profile_params)
   #   @profiles.user_id = current_user.id
--- film_master/config/routes.rb
+++ film_master/config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
  	get "/home" => "pages#home"
 	root "pages#home"
 	
+	#profile page and edits to account and profile
 	get "profiles/:profile_id" => "profiles#show", as: :profile
+	get "profiles/:profile_id/edit-profile" => "profiles#edit", as: :edit
+	patch "/profiles/:profile_id" => "profiles#update"
 
 	#create page to show all movies
 	get "/movies" => "movies#index"
