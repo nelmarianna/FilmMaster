429341c1cfe6cb2c7da9968e6800339e155c4cc6
nelmarianna <aniela.opolski@gmail.com>
2016-04-10 14:40:57 -0400

    helping lucia

--- film_master/app/models/search.rb
+++ film_master/app/models/search.rb
@@ -3,11 +3,11 @@ class Search < ActiveRecord::Base
 	def search_movies
 
 		movies = Movie.all 
-	#	mg= MoviesGenre.all
-		genre = Genre.all 
+		
 		movies = movies.where(["name ILIKE ?", "%#{keywords}%"]) if keywords.present?
 		#movies = movies.where(["gName ILIKE ?", gName]) if gName.present?
-		movies = movies.joins(:genres).where(["genre.gName ILIKE ?", gName]) if gName.present?
+		movies = movies.joins(:genres)
+		
 
 		#for the above... movie doesnt have a gName category.. thus it needs to be related to its
 		#relation table.. lol
